0a946dad87c09c1f9d6752a503436dd1563766a8
wloszynski <adrian@wloszynski.pl>
2021-01-12 17:27:12 +0100

    h2 title

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -3,7 +3,11 @@ import React from "react";
 import classes from "./Title.module.css";
 
 const title = () => {
-  return <div className={classes.Title}>statsjustjoin.it</div>;
+  return (
+    <div className={classes.Title}>
+      <h2>statsjustjoin.it</h2>
+    </div>
+  );
 };
 
 export default title;
