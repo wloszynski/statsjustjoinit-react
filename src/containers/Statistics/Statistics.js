a39d262e654dea12621a8ce0131c7ac18cf35c63
wloszynski <adrian@wloszynski.pl>
2021-01-13 18:09:44 +0100

    getting data from api

--- src/containers/Statistics/Statistics.js
+++ src/containers/Statistics/Statistics.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import StatisticsItems from "../../components/StatisticsItems/StatisticsItems";
 import StatisticsGraph from "../../components/StatisticsGraph/StatisticsGraph";
@@ -6,25 +7,18 @@ import StatisticsGraph from "../../components/StatisticsGraph/StatisticsGraph";
 import classes from "./Statistics.module.css";
 class Statistics extends Component {
   state = {
-    js: [
-      { id: "1", skill: "javascript", counter: "437" },
-      { id: "2", skill: "react", counter: "187" },
-      { id: "3", skill: "typescript", counter: "157" },
-      { id: "4", skill: "node.js", counter: "142" },
-      { id: "5", skill: "reactjs", counter: "120" },
-      { id: "6", skill: "css", counter: "109" },
-      { id: "7", skill: "html5", counter: "95" },
-      { id: "8", skill: "html", counter: "89" },
-      { id: "9", skill: "angular", counter: "67" },
-      { id: "10", skill: "vue.js", counter: "48" },
-      { id: "11", skill: "redux", counter: "43" },
-      { id: "12", skill: "css3", counter: "43" },
-      { id: "13", skill: "english", counter: "41" },
-      { id: "14", skill: "git", counter: "29" },
-      { id: "15", skill: "react native", counter: "26" },
-      { id: "16", skill: "aws", counter: "24" },
-    ],
+    item: null,
   };
+
+  componentDidMount() {
+    axios
+      .get(`https://api.jsonbin.io/b/5fff1cb668f9f835a3dec1a1`)
+      .then((res) => {
+        const items = res.data.js;
+        this.setState({ item: items });
+      });
+  }
+
   render() {
     return (
       <div className={classes.Statistics}>
@@ -33,7 +27,7 @@ class Statistics extends Component {
             <span>appearances</span>
           </div>
         </div>
-        <StatisticsItems items={this.state.js} />
+        <StatisticsItems items={this.state.item} />
         <StatisticsGraph />
       </div>
     );
