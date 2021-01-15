ea39bc95b08bc79b7c57e106dc241592bd551d90
wloszynski <adrian@wloszynski.pl>
2021-01-15 13:07:14 +0100

    imgs bundle

---
+++ src/imgs.js
@@ -0,0 +1,51 @@
+import all from "./img/all.svg";
+import javascript from "./img/javascript.svg";
+import html from "./img/html.svg";
+import php from "./img/php.svg";
+import ruby from "./img/ruby.svg";
+import python from "./img/python.svg";
+import java from "./img/java.svg";
+import net from "./img/net.svg";
+import scala from "./img/scala.svg";
+import c from "./img/c.svg";
+import mobile from "./img/mobile.svg";
+import testing from "./img/testing.svg";
+import devops from "./img/devops.svg";
+import ux from "./img/ux.svg";
+import pm from "./img/pm.svg";
+import game from "./img/game.svg";
+import analytics from "./img/analytics.svg";
+import security from "./img/security.svg";
+import data from "./img/data.svg";
+import go from "./img/go.svg";
+import sap from "./img/sap.svg";
+import support from "./img/support.svg";
+import other from "./img/other.svg";
+
+const imgs = [
+  all,
+  javascript,
+  html,
+  php,
+  ruby,
+  python,
+  java,
+  net,
+  scala,
+  c,
+  mobile,
+  testing,
+  devops,
+  ux,
+  pm,
+  game,
+  analytics,
+  security,
+  data,
+  go,
+  sap,
+  support,
+  other,
+];
+
+export default imgs;
