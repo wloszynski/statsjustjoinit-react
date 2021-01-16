aac3cb38d08325bebba5762b6e72ce9adfbb7c07
wloszynski <adrian@wloszynski.pl>
2021-01-16 11:59:29 +0100

    problems solved

--- src/components/SearchSidebar/SearchSidebar.js
+++ src/components/SearchSidebar/SearchSidebar.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import FullScreen from "../UI/FullScreen/FullScreen";
-=======
-import React from "react";
->>>>>>> c4b4f807857b594e4ea1820522a7ba5fc4e82e48
-
 const searchSidebar = () => {
   return null;
 };
