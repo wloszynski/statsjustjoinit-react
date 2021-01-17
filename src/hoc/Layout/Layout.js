454d87ac56cf79a54163ae9aab193305f33a521a
wloszynski <adrian@wloszynski.pl>
2021-01-17 14:36:33 +0100

    search deleted from layout

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -6,13 +6,12 @@ import Navigation from "../../containers/Navigation/Navigation";
 import Statistics from "../../containers/Statistics/Statistics";
 
 import Sidebar from "../../components/Sidebar/Sidebar";
-import SearchSidebar from "../../components/SearchSidebar/SearchSidebar";
+
 class Layout extends Component {
   render() {
     return (
       <Fragment>
         <Sidebar />
-        <SearchSidebar />
         <Header />
         <Navigation />
         <Switch>
